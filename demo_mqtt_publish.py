c7cf1a0fc55c7c176c6b5a6c8093c59832f7a97a
Simeon Tsvetanov <simeon.tsvetanov@gmail.com>
2021-03-07 17:51:17 +0200

    Added more commands

--- demo_mqtt_publish.py
+++ demo_mqtt_publish.py
@@ -5,7 +5,22 @@ broker_address = credentials.get('host')
 username = credentials.get('username')
 password = credentials.get('password')
 
+base_path = 'controllers/controller_id'
+
 client = mqtt.Client("P1")
 client.username_pw_set(username, password)
 client.connect(broker_address)
-client.publish("test", "OFF")
+client.publish(f'{base_path}/$name', 'Solar Controller')
+client.publish(f'{base_path}/$state', 'ready')
+client.publish(f'{base_path}/$notes', 'testnode')
+client.publish(f'{base_path}/testnode/$name', 'Temp Sensor')
+client.publish(f'{base_path}/testnode/$type', 'Type')
+client.publish(f'{base_path}/testnode/$properties', 'switchy')
+client.publish(f'{base_path}/testnode/switchy/$name', 'My switchy')
+
+client.publish(f'{base_path}/testnode/switchy/$setable', 'true')
+client.publish(f'{base_path}/testnode/switchy/$datatype', 'boolean')
+client.publish(f'{base_path}/testnode/switchy/$name', 'My switchy')
+
+client.publish(f'{base_path}/$homie', '3.0')
+client.publish(f'{base_path}/$stats/interval', '60')
